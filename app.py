20d2b9d818b079c2d994eb4bc95b8614febc1389
dhrubo006 <dhrubo006@gmail.com>
2023-12-02 22:11:50 +0600

    modified app.py, moved the upload function to app.py, it now records audio and sends to backend for transcription. But it does not show the transcribed text in the frontend.

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
-from src.upload_audio import upload_audio
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+from src.process_audio import process_audio
 
 app = Flask(__name__)
+app.secret_key = "unique"
 
 # Configuration
 app.config["UPLOAD_FOLDER"] = "uploads"
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024  # 16 MB max file size
 ALLOWED_EXTENSIONS = {"wav", "mp3", "ogg"}
+
+
+def allowed_file(filename):
+    """Check if the uploaded file has an allowed extension."""
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/")
@@ -15,9 +23,36 @@ def upload_file():
     return render_template("upload.html")
 
 
-@app.route("/uploader", methods=["GET", "POST"])
+@app.route("/uploader", methods=["POST", 'GET'])
 def handle_upload():
-    return upload_audio(app, ALLOWED_EXTENSIONS, app.config["UPLOAD_FOLDER"])
+    if "file" not in request.files:
+        flash("No file part")
+        return redirect(request.url)
+    file = request.files["file"]
+
+    if file.filename == "":
+        flash("No selected file")
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        if not os.path.exists(app.config["UPLOAD_FOLDER"]):
+            os.makedirs(app.config["UPLOAD_FOLDER"])
+        audio_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+        file.save(audio_path)
+
+        
+        try:
+            print("trying to trnscribe")
+            transcription = process_audio(audio_path)
+            print("transcription Done")
+            return transcription
+        except Exception as e:
+            flash("Error processing audio file")
+            return redirect(url_for("upload_file"))
+    else:
+        flash("File type not allowed")
+        return redirect(request.url)
 
 
 if __name__ == "__main__":
